9db786ccbafa08743c14759c54880732cc053470
tinak08 <tinukalbende@gmail.com>
2018-04-26 01:44:10 +0530

    first commit

---
+++ src/CompareNum.java
@@ -0,0 +1,20 @@
+
+public class CompareNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] ArrNum = {2,3,6,8,10,15,4,7};
+		int totalLen = ArrNum.length;
+		boolean Result ;
+		for(int i=0;i<totalLen-1;i++){
+			for(int j=i+1;j<totalLen;j++){
+				int Sum = ArrNum[i] + ArrNum[j];
+				if(Sum == 10){
+					Result = true;
+					System.out.println("The Sum of "+ArrNum[i]+" "+ArrNum[j]+" "+Result);
+				}
+			}
+		}
+	}
+
+}
